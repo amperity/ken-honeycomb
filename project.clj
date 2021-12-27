86db4bc23b2898fe3e9e2c5aa97e0ae117789e0f
Drew Inglis <drew@drewinglis.com>
2021-12-27 15:44:21 -0800

    Update dependencies to latest stable versions (#2)

--- project.clj
+++ project.clj
@@ -8,18 +8,18 @@
   [[lein-cloverage "1.2.2"]]
 
   :dependencies
-  [[org.clojure/clojure "1.10.1"]
-   [com.amperity/ken "1.0.0"]
+  [[org.clojure/clojure "1.10.3"]
+   [com.amperity/ken "1.0.1"]
    [com.stuartsierra/component "1.0.0"]
    [io.honeycomb.libhoney/libhoney-java "1.4.0"]]
 
   :profiles
   {:dev
    {:dependencies
-    [[org.slf4j/slf4j-nop "1.7.25"]]}
+    [[org.slf4j/slf4j-nop "1.7.32"]]}
 
    :repl
    {:source-paths ["dev"]
     :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
     :dependencies
-    [[org.clojure/tools.namespace "1.1.0"]]}})
+    [[org.clojure/tools.namespace "1.2.0"]]}})
